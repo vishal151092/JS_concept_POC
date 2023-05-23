f77bf53caca3d8bfcb42278662654988b6cff8a4
Vishal Singh <vishal151092@gmail.com>
2023-05-23 22:59:16 +0530

    Create ES6.js

---
+++ ES6.js
@@ -0,0 +1,78 @@
+/*
+* UNCOMMENT function to test
+*/
+
+
+
+// let user = {
+//     age :30,
+//     dept : "Admin"
+// }
+
+// var x = `User from ${user.dept} has age ${user.age}. `;
+
+// var y = "User from "+dept +" has age "+ age+ "."
+
+
+
+// function test(name = 'unknown'){
+
+//         console.log(`User name is ${name}`);
+// }
+
+// test();
+
+// function sum(b=0,c=0,d=0,...a){
+    
+//     let result=b+c+d;
+//     for(let val of a){
+//         result += val;
+//     }
+//     return result;
+// }
+
+// console.log(sum(2,3));
+
+// let a = {
+//     name : "user",
+//     age : 30,
+//     address : "Delhi"
+// };
+
+// let b = {
+//     dept : "Admin",
+//     org : "prism",
+    
+// }
+
+// for(let key in b){
+//     a[key] = b[key];
+// }
+// console.log(a);
+
+
+
+// a = {...a, ...b};
+
+// console.log(a);
+
+
+// let a = [1,2,3,4];
+
+// let b = [10,11,12]
+// a = [...a, ...b];
+
+// console.log(a);
+
+
+
+// let data = {
+//     name : "User",
+//     age : 30
+// }
+
+
+// let {name: userName, age: userAge} = data;
+
+// console.log(userName);
+// console.log(userAge);
